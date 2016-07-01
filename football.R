f34040817386a30a255774f7d84710ea5fa78103
Atanas Mirchev <taimir93@gmail.com>
2016-07-01 20:11:02 +0200

    Further basic ideas for modelling

--- football.R
+++ football.R
@@ -1,8 +1,38 @@
 
 # Load the data into R
 playoffs = read.csv("data/EM2004.2012.publ.txt", header=TRUE, sep=" ")
+playoffs$Heim = as.factor(playoffs$Heim)
+# structural attibutes
+# EM
+
+playoffs$id = NULL
+playoffs$Max1 = NULL
+playoffs$Max2 = NULL
+playoffs$Max1_gegner = NULL
+playoffs$Max2_gegner = NULL
+playoffs$Grenzland = NULL
+playoffs$Grenzland_gegner = NULL
+playoffs$Legionaere = NULL
+playoffs$Legionaere_gegner = NULL
+playoffs$FIFA.Punkte = NULL
+playoffs$FIFA.Punkte_gegner = NULL
+playoffs$Population = NULL
+playoffs$Population_gegner = NULL
+playoffs$Alter.Trainer = NULL
+playoffs$Alter.Trainer_gegner = NULL
 
 
+# Important attributes:
+# Tore, Team, Gegner, Heim, Quoten, Marktwert, Fifa.Platz, UEFA.Platz, UEFA.Punkte, CL.Spieler, 
+# UEFA.Cup.Spieler, 
+
+# attributes in question: GDP, Altersschnitt, FIFA.Punkte, Alter.Trainer
+
+# Unused attributes:
+# Max1, Max2, Grenzland, Legionaere, Population, Population_gegner
+
+# To the teams: add some indicator of the class of the
+# goal keeper, goal maker, trainer
 
 odds = read.csv("data/QuotenEM2016.txt", header=TRUE, sep="\t")
 
@@ -17,10 +47,16 @@ fifa16 = read.csv("data/FIFA2016.txt", header=TRUE, sep="\t")
 
 uefa16 = read.csv("data/UEFA2016.txt", header=TRUE, sep="\t")
 
-uefa16$Startplätze = NULL
-
 neues16 = read.csv("data/Neues2016.txt", header=TRUE, sep="\t")
 names(neues16) = c("team", "champ_league", "uefa_cup", "Grenzland", "trainer_age", "trainer_nation", "players_abroad", "max1", "max2")
 View(neues16)
 
+
+# Detected tendencies:
+# plot(playoffs$Tore, playoffs$UEFA.Platz) ---> Only teams with high UEFA scores actually 
+# manage to score more goals in the UEFA tournament.
+
+
+# Ideas:
+# try to come up with a probabilistic intuition behind weighting the years of the presented championships
 
